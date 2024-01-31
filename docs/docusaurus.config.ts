85077cdd59639218ebda318293cf1b6d5e69a122
Netanel Gilad <netanelg@wix.com>
2024-01-31 16:47:44 +0200

    add docs website

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,125 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "Wix Smarts",
+  tagline: "The React framework for the Wix Developer Platform",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://wix-smarts-docs.vercel.app",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/wix-incubator/smarts/tree/main/docs",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/wix-incubator/smarts/tree/main/docs",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    // image: "img/docusaurus-social-card.jpg",
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: "Wix Smarts",
+      logo: {
+        alt: "Wix Smarts Logo",
+        src: "img/logo.svg",
+        srcDark: "img/logo-dark.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "docsSidebar",
+          position: "left",
+          label: "Docs",
+        },
+        {
+          href: "https://github.com/wix-incubator/smarts",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Introduction",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Discord",
+              href: "https://discord.gg/devs-on-wix",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/DevsOnWix",
+            },
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/wix-smarts",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/wix-incubator/smarts",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Wix, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
--- packages/e2e-spec/cypress/e2e/stores/product-page.cy__.ts
+++ packages/e2e-spec/cypress/e2e/stores/product-page.cy__.ts
@@ -1,127 +1,129 @@
-describe('product page', () => {
+describe("product page", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/server/store/products/acme-circles-t-shirt');
+    cy.visit(
+      "http://localhost:3000/server/store/products/acme-circles-t-shirt",
+    );
     // wait for hydration sadly
     cy.wait(200);
   });
 
-  it('should display the product page components', () => {
-    container('ProductTitle').should('contain', 'Acme Circles T-Shirt');
+  it("should display the product page components", () => {
+    container("ProductTitle").should("contain", "Acme Circles T-Shirt");
 
-    container('ProductDescription').should(
-      'contain',
-      '60% combed ringspun cotton/40% polyester jersey tee.',
+    container("ProductDescription").should(
+      "contain",
+      "60% combed ringspun cotton/40% polyester jersey tee.",
     );
 
-    container('ProductPrice').should('contain', '€20.00');
+    container("ProductPrice").should("contain", "€20.00");
 
-    container('ProductMainMedia')
-      .findByAltText('t-shirt-circles-blue.png')
+    container("ProductMainMedia")
+      .findByAltText("t-shirt-circles-blue.png")
       .should(
-        'have.attr',
-        'src',
-        'https://static.wixstatic.com/media/8dfd06_3e3feaf389cf47fd9c781e5977a89c3d~mv2.png/v1/fit/w_3023,h_3000,q_90/file.png',
+        "have.attr",
+        "src",
+        "https://static.wixstatic.com/media/8dfd06_3e3feaf389cf47fd9c781e5977a89c3d~mv2.png/v1/fit/w_3023,h_3000,q_90/file.png",
       );
 
-    container('ProductMediaItems')
-      .findByAltText('t-shirt-circles-blue.png')
+    container("ProductMediaItems")
+      .findByAltText("t-shirt-circles-blue.png")
       .should(
-        'have.attr',
-        'src',
-        'https://static.wixstatic.com/media/8dfd06_3e3feaf389cf47fd9c781e5977a89c3d~mv2.png/v1/fit/w_3023,h_3000,q_90/file.png',
+        "have.attr",
+        "src",
+        "https://static.wixstatic.com/media/8dfd06_3e3feaf389cf47fd9c781e5977a89c3d~mv2.png/v1/fit/w_3023,h_3000,q_90/file.png",
       );
 
-    container('ProductMediaItems')
-      .findByAltText('t-shirt-circles-white.png')
+    container("ProductMediaItems")
+      .findByAltText("t-shirt-circles-white.png")
       .should(
-        'have.attr',
-        'src',
-        'https://static.wixstatic.com/media/8dfd06_e7fbbb177afd4ec086f479a72cdccecc~mv2.png/v1/fit/w_3023,h_3000,q_90/file.png',
+        "have.attr",
+        "src",
+        "https://static.wixstatic.com/media/8dfd06_e7fbbb177afd4ec086f479a72cdccecc~mv2.png/v1/fit/w_3023,h_3000,q_90/file.png",
       );
 
-    container('ProductMediaItems')
-      .findByAltText('t-shirt-circles-black.png')
+    container("ProductMediaItems")
+      .findByAltText("t-shirt-circles-black.png")
       .should(
-        'have.attr',
-        'src',
-        'https://static.wixstatic.com/media/8dfd06_99d45fc8dafa4f06835fffa4ac83e209~mv2.png/v1/fit/w_3023,h_3000,q_90/file.png',
+        "have.attr",
+        "src",
+        "https://static.wixstatic.com/media/8dfd06_99d45fc8dafa4f06835fffa4ac83e209~mv2.png/v1/fit/w_3023,h_3000,q_90/file.png",
       );
 
-    container('SelectedProductVariantProvider').should('exist');
+    container("SelectedProductVariantProvider").should("exist");
 
-    container('ProductVariantSelector')
-      .findByRole('group', { name: 'Color' })
-      .findAllByRole('radio')
+    container("ProductVariantSelector")
+      .findByRole("group", { name: "Color" })
+      .findAllByRole("radio")
       .should(($size) => {
         expect($size).to.have.length(3);
 
         [
-          { label: 'Black' },
-          { label: 'White' },
-          { label: 'Blue', disabled: true },
+          { label: "Black" },
+          { label: "White" },
+          { label: "Blue", disabled: true },
         ].forEach(({ label, disabled }, index) => {
           if (disabled) {
-            expect($size.eq(index)).to.have.attr('disabled');
+            expect($size.eq(index)).to.have.attr("disabled");
           } else {
-            expect($size.eq(index)).not.to.have.attr('disabled');
+            expect($size.eq(index)).not.to.have.attr("disabled");
           }
 
           expect(
-            Cypress.$(`label[for="${$size.eq(index).attr('id')}"]`),
+            Cypress.$(`label[for="${$size.eq(index).attr("id")}"]`),
           ).to.contain(label);
         });
       });
 
-    container('ProductVariantSelector')
-      .findByRole('group', { name: 'Size' })
-      .findAllByRole('radio')
+    container("ProductVariantSelector")
+      .findByRole("group", { name: "Size" })
+      .findAllByRole("radio")
       .should(($size) => {
         expect($size).to.have.length(7);
 
-        ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'].forEach((size, index) => {
+        ["XS", "S", "M", "L", "XL", "XXL", "XXXL"].forEach((size, index) => {
           expect(
-            Cypress.$(`label[for="${$size.eq(index).attr('id')}"]`),
+            Cypress.$(`label[for="${$size.eq(index).attr("id")}"]`),
           ).to.contain(size);
         });
       });
 
-    container('AddToCurrentCartQuantitySelector')
-      .findByRole('spinbutton')
-      .should('have.value', '1');
+    container("AddToCurrentCartQuantitySelector")
+      .findByRole("spinbutton")
+      .should("have.value", "1");
 
-    container('AddProductVariantToCartButton')
-      .findByRole('button')
-      .should('be.disabled')
-      .should('contain', 'Add to cart');
+    container("AddProductVariantToCartButton")
+      .findByRole("button")
+      .should("be.disabled")
+      .should("contain", "Add to cart");
 
-    container('ProductVariantSelector')
-      .findByRole('group', { name: 'Color' })
-      .findByRole('radio', { name: 'Black' })
+    container("ProductVariantSelector")
+      .findByRole("group", { name: "Color" })
+      .findByRole("radio", { name: "Black" })
       .click();
 
-    container('ProductVariantSelector')
-      .findByRole('group', { name: 'Size' })
-      .findByLabelText('M')
-      .should('be.disabled');
+    container("ProductVariantSelector")
+      .findByRole("group", { name: "Size" })
+      .findByLabelText("M")
+      .should("be.disabled");
 
-    container('ProductVariantSelector')
-      .findByRole('group', { name: 'Size' })
-      .findByRole('radio', { name: 'L' })
+    container("ProductVariantSelector")
+      .findByRole("group", { name: "Size" })
+      .findByRole("radio", { name: "L" })
       .click();
 
-    container('ProductVariantSelector')
-      .findByRole('group', { name: 'Color' })
-      .findByLabelText('White')
-      .should('be.disabled');
+    container("ProductVariantSelector")
+      .findByRole("group", { name: "Color" })
+      .findByLabelText("White")
+      .should("be.disabled");
 
-    container('AddToCurrentCartQuantitySelector')
-      .findByRole('spinbutton')
-      .type('{backspace}2');
+    container("AddToCurrentCartQuantitySelector")
+      .findByRole("spinbutton")
+      .type("{backspace}2");
   });
 });
 
 function container(name: string) {
-  return cy.findByRole('group', {
+  return cy.findByRole("group", {
     name,
   });
 }
